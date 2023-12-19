cc2ff233886e68b84df970b4f7a9414fee1a605a
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-12-19 12:36:57 +0900

    FF

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 
 
@@ -9,6 +9,9 @@ import './App.css';
   //  todos 초기값을 빈 배열로 생성하겠다는 의미
   //todos 는 할 일 목록 들을 저장하는 공간
   const [todos, setTodos] = useState([]);
+
+
+
 
 
   //newTodo 새로운 할 일을 추가 작성할 수 있는 공간
@@ -34,16 +37,29 @@ import './App.css';
     const updateTodos = [...todos];
     //복사된 배열에서 지정된 자리값(index)를 1개 제거하겠다는 의미
     //updateTodos : 복사된 배열
-    // slice : 제거
+
+    // slice splice : 배열 조작
+    // slice : 배열 일부분 복사해서 새로운 배열 반환
+    // 원본에 있던 배열은 변경되지 않지만 시작 - 종료 인덱스
+
+    // splice : 배열의 내용을 수정하거나 삭제하고, 필요에 따라
+    // 새로운 요소를 추가할 수 있음
+
     updateTodos.splice(index, 1);
     // 내가 제거하고 싶은 할 일을 제거한 후 
     // setTodos를 활용해서 할일 목록을 재설정
     setTodos(updateTodos);
   };
 
+  useEffect(() => {
+    console.log('todos 변경됨 : ', todos);
+  }, [todos]);
+
+
   return (
     <div>
       <h2>useState 활용한 할 일 추가하기</h2>
+      <img ></img>
       <div>
         <input
           type="text"
@@ -65,3 +81,33 @@ import './App.css';
 };
 
 export default TodoList;
+
+//my-app 
+// map 배열 객체 메서드
+// 배열의 각 요소에서 함수를 호출하고, 그 함수의 반환값으로
+// 새로운 배열을 생성
+/*
+예제코드 : 
+  const newArray = array.map((value, index, array) => {});
+
+  //value : 현재 배열 안에 있는 배열의 요소
+  //index : 배열 안에 있는 자리값
+  //array : 배열의 원본
+  //반환값{} : 새로운 배열에서 해당하는 인덱스에 들어갈 값
+
+
+  {todos.map((todo, index) => (
+    //key={index} map 함수로 요소를 생성할 때 
+    //각 요소에 고유한 key 값을 지정
+    // react 각 값에 대한 번호를 부여함으로써 목록을 부여된 번호로
+    //추적하기 위해 사용됨
+    <li key={index}> 
+      {todo}
+      <button onClick={()=>removeTodo(index)}>삭제하기</button>
+    </li>
+  ))}
+
+  array.map((요소, 자리값,만약 새로운 배열이 필요하다면 배열 값 넣어줌)=> {
+    //리턴값으로 새로운 배열을 구성
+  });
+  */
